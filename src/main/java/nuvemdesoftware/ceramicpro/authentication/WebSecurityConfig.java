c7b1474c0fcd77da562a5e26a95403dbaa53a66b
Adérito Oliveira <aderito.nelson1@gmail.com>
2023-10-29 14:55:28 +0000

    Create WebSecurityConfig to resolve dependencies from authenticationManager created after upgrade to Spring 3

---
+++ src/main/java/nuvemdesoftware/ceramicpro/authentication/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package nuvemdesoftware.ceramicpro.authentication;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
