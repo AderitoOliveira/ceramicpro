82b858b1f5b756e05b45f394c94e20750cdcbd3b
Adérito Oliveira <aderito.nelson1@gmail.com>
2024-02-17 16:47:53 +0000

    [ADD] - Create all the necessary objects for dealing with the Refresh Token functionality

---
+++ src/main/java/nuvemdesoftware/ceramicpro/model/RefreshToken.java
@@ -0,0 +1,37 @@
+package nuvemdesoftware.ceramicpro.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.Optional;
+
+/**
+ * @author mhmdz
+ * Created By Zeeshan on 20-05-2023
+ * @project oauth-jwt
+ */
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String token;
+
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User userInfo;
+
+}
